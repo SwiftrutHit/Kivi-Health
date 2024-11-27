b65b148c8dfe7a900bdbdac8e29ee6a4458f7cc5
SwiftrutHit <hit.morker@swiftrut.com>
2024-11-27 10:47:53 +0530

    Added New sections in the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,7 @@ $('.process-slider').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 2,
                 slidesToScroll: 3,
                 infinite: true,
                 dots: true
@@ -56,16 +56,9 @@ $('.process-slider').slick({
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 1,
                 slidesToScroll: 2
             }
         },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
     ]
 });
