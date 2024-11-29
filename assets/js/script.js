c0051ad0147c69da65e1e69d86491ac3953ee1c6
SwiftrutHit <hit.morker@swiftrut.com>
2024-11-30 04:31:50 +0530

    Entire project is done. still wana to add effects and extra features for enhancement

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $('.speciality-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 2000,
     arrows: false,
     responsive: [
         {
@@ -13,15 +13,22 @@ $('.speciality-slider').slick({
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                // infinite: true,
+                // dots: true
+            }
+        },
+        {
+            breakpoint:768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         },
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         },
         {
@@ -50,16 +57,53 @@ $('.process-slider').slick({
                 slidesToShow: 2,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                // dots: true
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 800,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 2
+                slidesToScroll: 1
             }
         },
     ]
 });
+// $('.doctors-slider').slick({
+//     dots: false,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2500,
+//     arrows: true,
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+
+//             }
+//         },
+//         {
+//             breakpoint: 800,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2,
+//                 arrows: false,
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 arrows: false,
+//             }
+//         },
+//     ]
+// });
 // accordian js
